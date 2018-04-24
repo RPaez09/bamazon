0a5f6a50b18a8481bdb12e7e600a854c199534bc
Richard Paez <Richardp809@gmail.com>
2018-04-24 01:08:34 -0400

    fixed typo in query

--- schema.sql
+++ schema.sql
@@ -3,13 +3,14 @@ DROP DATABASE IF EXISTS bamazon_DB;
 CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
-CREATE TABLE products(
+CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(80) NOT NULL,
     manufacturer_name VARCHAR(25) NOT NULL,
     department_name VARCHAR(25) NOT NULL,
     price DECIMAL(6,2) NOT NULL,
-    stock_quantity INT NOT NULL
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
 -- Mock data
